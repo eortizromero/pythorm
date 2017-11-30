bfdb0c87f7fe1ef431ff99445b3e97acd0bac20d
Edgar Ortiz <edgardo.ortiz@desiteg.com>
2017-11-30 10:54:22 -0600

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from functools import setup
+
+setup(
+        name = 'PythORM',
+        version = '1.0',
+        description = 'ORM for Python',
+        author = 'Edgar Ortiz',
+        author_emain = 'edgardoficial.yo@gmail.com',
+        url = 'https://github.com/eortiztromero/pythorm.git',
+        install_requires = ['psycopg2'],
+    )
